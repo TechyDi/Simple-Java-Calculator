2dec541c379f511421a2372281e76654a936c1f9
TechyDi <dkd121020@gmail.com>
2024-08-04 17:33:33 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 public class calculator{
     public static void main(String[] args){
         System.out.println("CALCULATOR PROJECT USING JAVA.");
-
+        System.out.println("CALCULAOTR PROJECT WRITTEN IN GITHUB.");
     }
 }
