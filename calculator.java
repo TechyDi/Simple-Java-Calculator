f3445a204d813ad454a5dba2b3ac60c3e6d48e54
TechyDi <dkd1210202gmail.com>
2024-08-04 17:51:29 +0530

    TRIAL COMMIT

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 public class calculator{
     public static void main(String[] args){
         System.out.println("CALCULATOR PROJECT USING JAVA.");
-
+        System.out.println("NEW TRIAL LINE.");
     }
 }
