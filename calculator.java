2f801db4b76390cc113de2dc5168215cc330167b
TechyDi <dkd1210202gmail.com>
2024-08-04 19:29:09 +0530

    Calculator project complete.

--- calculator.java
+++ calculator.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class calculator{
     public static void main(String[] args){
-        System.out.println("CALCULATOR PROJECT USING JAVA.");
-        System.out.println("CALCULAOTR PROJECT WRITTEN IN GITHUB.");
-        
+    // Simple Calculator using JAVA
+
+    float num1,num2,Result; //Taking num1 & num2 for first and seconnd number respectiely, Result for the output after the operation.
+    char operator;
+     Scanner sc = new Scanner(System.in); //Adding scanner class for user input
+        System.out.println("ENTER TWO NUMBERS TO CALCULATE:- ");       
+         
+        System.out.println("First Number is:- ");
+         num1 = sc.nextFloat();
+
+        System.out.println("Second Number is:- ");
+         num2 = sc.nextFloat();
+
+         System.out.print("CHOOSE YOUR OPERATOR   +, - , * , / \n");
+         operator = sc.next().charAt(0);
+         switch (operator) {
+            case '+':
+            Result= num1+num2;
+                System.out.println(Result);
+            break;
+
+            case '-':
+            Result= num1-num2;
+            System.out.println(Result);
+            break;
+
+            case '*':
+            Result= num1*num2;
+            System.out.println(Result);
+            break;
+
+            case '/':
+            Result= num1/num2;
+            System.out.println(Result);
+            break;
+            default:
+                break;
+         }
+         sc.close();
+
     }
 }
